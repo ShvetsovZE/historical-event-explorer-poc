d7f198621f5085f624883655382f4019d4c3e6c4
Zhan Shvetsov <saferon31rus@gmail.com>
2024-01-19 13:05:21 +1300

    Adds work around to stop startup execution of the service

--- HistoricalEventExporter/Common/EventExporter.cs
+++ HistoricalEventExporter/Common/EventExporter.cs
@@ -3,11 +3,12 @@ using Microsoft.Extensions.Logging;
 
 namespace HistoricalEventExporter.Exporters
 {
-    public class EventExporter<T> : IEventExporter<T>, IHostedService
+    public class EventExporter<T> : BackgroundService, IEventExporter<T>
     {
         private readonly ILogger<EventExporter<T>> _logger;
         private readonly IEventDataReader<T> _eventDataReader;
         private readonly IEventPublisher<T> _eventPublisher;
+        private bool _ranManually = false; // added to skip startup execution
 
         public EventExporter(
             IEventDataReader<T> eventDataReader,
@@ -19,30 +20,13 @@ namespace HistoricalEventExporter.Exporters
             _logger = logger;
         }
 
-        public async Task StartAsync(CancellationToken cancellationToken)
-        {
-            _logger.LogInformation($"Starts Hosted service for {typeof(T).Name} event type export");
-
-            var events = await _eventDataReader.GetDataAsync();
-            _logger.LogInformation($"Found {events.Count()} events of type {typeof(T).Name}");
-            
-            await _eventPublisher.PublishEventsAsync(events);
-
-           await StopAsync(cancellationToken);
-
-        }
 
         public async Task StartExportAsync()
         {
             _logger.LogInformation($"Starts Exporting events type: {typeof(T).Name}");
+            _ranManually = true;
 
-            await StartAsync(new CancellationToken());
-        }
-
-        public async Task StopAsync(CancellationToken cancellationToken)
-        {
-            _logger.LogInformation($"Stops Hosted service for {typeof(T).Name} event type export");
-
+            await ExecuteAsync(new CancellationToken());
         }
 
         public async Task StopExportAsync()
@@ -51,5 +35,22 @@ namespace HistoricalEventExporter.Exporters
             _logger.LogInformation($"Stops Exporting events type: {typeof(T).Name}");
         }
 
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            if (_ranManually)
+            {               
+                    _logger.LogInformation($"Starts Background service for {typeof(T).Name} event type export");
+
+                    var events = await _eventDataReader.GetDataAsync();
+                    _logger.LogInformation($"Found {events.Count()} events of type {typeof(T).Name}");
+
+                    await _eventPublisher.PublishEventsAsync(events);
+                _logger.LogInformation($"Finished exporting events of type {typeof(T).Name}");
+            }
+            else
+            {
+                _logger.LogInformation($"Skipping startup execution");
+            }
+        }
     }
 }
